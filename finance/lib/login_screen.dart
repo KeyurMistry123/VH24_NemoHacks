0c4aa24502d05093f1b7d402122c7667565fe888
itsadrita <143730997+itsadrita@users.noreply.github.com>
2024-10-04 13:25:34 +0530

    first commit

---
+++ finance/lib/login_screen.dart
@@ -0,0 +1,218 @@
+import 'package:finance/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+class LoginPage extends StatefulWidget {
+  // Add the 'key' parameter to the constructor
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  String? _email, _password;
+
+  Future<void> _login() async {
+    if (_formKey.currentState?.validate() == true) {
+      _formKey.currentState?.save();
+      final localContext = context; // Capture the context before the async gap
+      try {
+        await _auth.signInWithEmailAndPassword(
+            email: _email!, password: _password!);
+        Navigator.pushReplacement(
+          localContext, // Use the captured context
+          MaterialPageRoute(
+              builder: (context) =>
+                  SignUpPage()), // Navigate to VideosScreen
+        );
+      } on FirebaseAuthException catch (e) {
+        print(e.message);
+        ScaffoldMessenger.of(localContext)
+            .showSnackBar(// Use the captured context
+                SnackBar(content: Text(e.message ?? 'Unknown error occurred')));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.green.shade900,
+                Colors.green.shade900,
+                Colors.green.shade900,
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 80,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeInUp(
+                        duration: Duration(milliseconds: 1000),
+                        child: Text("Login",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 40))),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    FadeInUp(
+                        duration: Duration(milliseconds: 1300),
+                        child: Text("Welcome Back",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18))),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60)),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 60,
+                          ),
+                          FadeInUp(
+                            duration: Duration(milliseconds: 1400),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10),
+                                  )
+                                ],
+                              ),
+                              child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.black)),
+                                      ),
+                                      child: TextFormField(
+                                        validator: (input) =>
+                                            input?.isEmpty == true
+                                                ? 'Enter your email'
+                                                : null,
+                                        onSaved: (input) => _email = input,
+                                        decoration: InputDecoration(
+                                          hintText: "Email or Phone number",
+                                          hintStyle:
+                                              TextStyle(color: Colors.black),
+                                          border: InputBorder.none,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.black)),
+                                      ),
+                                      child: TextFormField(
+                                        obscureText: true,
+                                        validator: (input) =>
+                                            input?.isEmpty == true
+                                                ? 'Enter your password'
+                                                : null,
+                                        onSaved: (input) => _password = input,
+                                        decoration: InputDecoration(
+                                          hintText: "Password",
+                                          hintStyle:
+                                              TextStyle(color: Colors.black),
+                                          border: InputBorder.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          FadeInUp(
+                              duration: Duration(milliseconds: 1500),
+                              child: Text("Forgot Password?",
+                                  style: TextStyle(color: Colors.black))),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          FadeInUp(
+                            duration: Duration(milliseconds: 1600),
+                            child: MaterialButton(
+                              onPressed: _login,
+                              height: 50,
+                              color: Colors.green.shade900,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: Center(
+                                child: Text("Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FadeInUp(
+                            duration: Duration(milliseconds: 1700),
+                            child: MaterialButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/signup');
+                              },
+                              child: Text("Don't have an account? Sign up",
+                                  style: TextStyle(color: Colors.black)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ finance/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:finance/login_screen.dart';
+import 'package:finance/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final Future<FirebaseApp> initialization = Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyA3AArnUovKnHDaV7KTEI2i91n55pW_wFs",
+      appId: "1:137430080200:android:819703b0ccebfabb3423fd",
+      messagingSenderId: "137430080200",
+      projectId: "fintech-vcet",
+     
+    ),
+  );
+
+  runApp(MyApp(initialization: initialization));
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> initialization;
+
+  // Add the 'key' parameter to the constructor
+  const MyApp({super.key, required this.initialization});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text("Initializing..."),
+              ),
+              body: const Center(
+                child: CircularProgressIndicator(), // Display during loading
+              ),
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text("Error initializing Firebase"),
+              ),
+              body: const Center(
+                child: Text("Something went wrong!"), // Display error message
+              ),
+            ),
+          );
+        }
+
+        return Builder(builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+       
+            initialRoute: '/login',
+            routes: {
+              
+              '/login': (context) => LoginPage(key: const Key("LoginScreen")),
+             
+              '/signup': (context) => SignUpPage(key: const Key("RegistrationScreen")),
+             
+            },
+            theme: ThemeData.dark(), // Ensure this is set correctly
+          );
+        });
+      },
+    );
+  }
+}
